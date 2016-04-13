09c4f7df04b1b92796cc5b811eb93c12f5c7e608
Jason R. McNeil <jason@jasonrm.net>
2016-04-13 14:33:58 -0700

    Push to private registry

--- build.sh
+++ build.sh
@@ -17,3 +17,10 @@ fi
 gpg --keyserver-options auto-key-retrieve --auto-key-locate pka --verify "archlinux-bootstrap-${VERSION}-x86_64.tar.gz.sig" "archlinux-bootstrap-${VERSION}-x86_64.tar.gz"
 
 docker run --rm -v $(pwd):/arch-bootstrap -v /var/run/docker.sock:/var/run/docker.sock --env http_proxy=${http_proxy} --env https_proxy=${https_proxy} ubuntu:latest /bin/bash /arch-bootstrap/build-image.sh $IMAGE_NAME $VERSION
+
+# Push to registry if configured
+if [ ! -z "${DOCKER_REGISTRY}" ]; then
+    docker login --username=${DOCKER_USER} --password=${DOCKER_PASS} --email=${DOCKER_EMAIL} ${DOCKER_REGISTRY}
+    docker tag "${IMAGE_NAME}:latest" "${DOCKER_REGISTRY}/${IMAGE_NAME}:latest"
+    docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:latest"
+fi
