cf53de4e65e83dbfa06a821376e49cdb5f8f9ed5
Jason R. McNeil <jason@jasonrm.net>
2016-06-05 08:42:43 +0000

    Attempt to try latest docker versions

--- build-image.sh
+++ build-image.sh
@@ -7,7 +7,11 @@ IMAGE_NAME=$1
 VERSION=$2
 
 apt-get update
-apt-get install docker.io -y
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get -y install docker-engine
 
 tar xf /arch-bootstrap/archlinux-bootstrap-${VERSION}-x86_64.tar.gz
 
