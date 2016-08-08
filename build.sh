b842192be26024be5ba0403f6e9daed2b39293af
Jason R. McNeil <jason@jasonrm.net>
2016-08-07 18:16:06 -0700

    Use docker image

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -o pipefail
 IMAGE_NAME="atomica/arch-bootstrap"
 MIRROR="http://mirrors.kernel.org/archlinux"
 VERSION=$(curl ${MIRROR}/iso/latest/ | grep -Poh '(?<=archlinux-bootstrap-)\d*\.\d*\.\d*(?=\-x86_64)' | head -n 1)
+DOCKER_VERSION=$(docker version --format '{{.Server.Version}}')
 
 if [[ ! -f "archlinux-bootstrap-${VERSION}-x86_64.tar.gz" ]]; then
     curl -O -L "${MIRROR}/iso/${VERSION}/archlinux-bootstrap-${VERSION}-x86_64.tar.gz"
@@ -16,7 +17,7 @@ fi
 
 gpg --keyserver-options auto-key-retrieve --auto-key-locate keyserver --keyserver pool.sks-keyservers.net --verify "archlinux-bootstrap-${VERSION}-x86_64.tar.gz.sig" "archlinux-bootstrap-${VERSION}-x86_64.tar.gz"
 
-docker run --rm -v $(pwd):/arch-bootstrap -v /var/run/docker.sock:/var/run/docker.sock --env http_proxy=${http_proxy} --env https_proxy=${https_proxy} ubuntu:16.04 /bin/bash /arch-bootstrap/build-image.sh $IMAGE_NAME $VERSION
+docker run --rm -v $(pwd):/arch-bootstrap -v /var/run/docker.sock:/var/run/docker.sock --env http_proxy=${http_proxy} --env https_proxy=${https_proxy} docker:$DOCKER_VERSION /bin/bash /arch-bootstrap/build-image.sh $IMAGE_NAME $VERSION
 
 # Push to registry if configured
 if [ ! -z "${DOCKER_REGISTRY}" ]; then
